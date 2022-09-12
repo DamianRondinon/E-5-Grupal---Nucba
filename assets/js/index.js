1d58129af38e7aac0ee252c912d0fade67de1a55
CristianMansilla <cristianezequielmansilla@hotmail.com>
2022-09-12 14:35:14 -0300

    Funcionalidad de filtrado

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,62 +1,359 @@
-let pizzas = [
-  {
-    nombre: "La MR. Pit",
-    descripcion: "Sólo para expertos",
-    precio: "$350",
-    imagen: "./assets/img/Photo Menu.png",
-  },
-  {
-    nombre: "¡Q'Jamone!",
-    descripcion: "c/jamón crudo",
-    precio: "$350",
-    imagen: "./assets/img/Photo Menu-1.png",
-  },
-  {
-    nombre: "La Charly García",
-    descripcion: "¡BASTA!",
-    precio: "$380",
-    imagen: "./assets/img/Photo Menu-2.png",
-  },
-  {
-    nombre: "La Maradona",
-    descripcion: "¡Eterna!",
-    precio: "$450",
-    imagen: "./assets/img/Photo Menu-3.png",
-  },
-  {
-    nombre: "Picantovich",
-    descripcion: "Pica 2 veces",
-    precio: "$350",
-    imagen: "./assets/img/Photo Menu-4.png",
-  },
-  {
-    nombre: "La Hasbulla",
-    descripcion: "En honor al 1",
-    precio: "$990",
-    imagen: "./assets/img/Photo Menu-5.png",
-  },
-  {
-    nombre: "Leo Messi",
-    descripcion: "¡De pie señores!",
-    precio: "$350",
-    imagen: "./assets/img/Photo Menu-6.png",
-  },
-  {
-    nombre: "Nick Gi",
-    descripcion: "La que desaparece",
-    precio: "Gratis",
-    imagen: "./assets/img/Photo Menu-7.png",
-  },
-];
+let popular = {
+  data:[
+    {
+      nombre: "La MR. Pit",
+      category: "Pizza",
+      descripcion: "Sólo para expertos",
+      precio: "$350",
+      imagen: "./assets/img/Photo Menu.png",
+    },
+    {
+      nombre: "¡Q'Jamone!",
+      category: "Pizza",
+      descripcion: "c/jamón crudo",
+      precio: "$350",
+      imagen: "./assets/img/Photo Menu-1.png",
+    },
+    {
+      nombre: "La Charly García",
+      category: "Pizza",
+      descripcion: "¡BASTA!",
+      precio: "$380",
+      imagen: "./assets/img/Photo Menu-2.png",
+    },
+    {
+      nombre: "La Maradona",
+      category: "Pizza",
+      descripcion: "¡Eterna!",
+      precio: "$450",
+      imagen: "./assets/img/Photo Menu-3.png",
+    },
+    {
+      nombre: "Picantovich",
+      category: "Pizza",
+      descripcion: "Pica 2 veces",
+      precio: "$750",
+      imagen: "./assets/img/Photo Menu-4.png",
+    },
+    {
+      nombre: "La Hasbulla",
+      category: "Pizza",
+      descripcion: "En honor al 1",
+      precio: "$990",
+      imagen: "./assets/img/Photo Menu-5.png",
+    },
+    {
+      nombre: "Leo Messi",
+      category: "Pizza",
+      descripcion: "¡De pie señores!",
+      precio: "$10",
+      imagen: "./assets/img/Photo Menu-6.png",
+    },
+    {
+      nombre: "Nick Gi",
+      category: "Pizza",
+      descripcion: "La que desaparece",
+      precio: "$Gratis",
+      imagen: "./assets/img/Photo Menu-7.png",
+    },
+  ]
+};
 
-localStorage.setItem("pizzas", JSON.stringify(pizzas)); // lo envío al local storage
+let products = {
+  data:[
+    {
+      nombre: "La MR. Pit",
+      category: "Pizza",
+      descripcion: "Sólo para expertos",
+      precio: "$350",
+      imagen: "./assets/img/Photo Menu.png",
+    },
+    {
+      nombre: "¡Q'Jamone!",
+      category: "Pizza",
+      descripcion: "c/jamón crudo",
+      precio: "$350",
+      imagen: "./assets/img/Photo Menu-1.png",
+    },
+    {
+      nombre: "La Charly García",
+      category: "Pizza",
+      descripcion: "¡BASTA!",
+      precio: "$380",
+      imagen: "./assets/img/Photo Menu-2.png",
+    },
+    {
+      nombre: "La Maradona",
+      category: "Pizza",
+      descripcion: "¡Eterna!",
+      precio: "$10",
+      imagen: "./assets/img/Photo Menu-3.png",
+    },
+    {
+      nombre: "Picantovich",
+      category: "Pizza",
+      descripcion: "Pica 2 veces",
+      precio: "$750",
+      imagen: "./assets/img/Photo Menu-4.png",
+    },
+    {
+      nombre: "La Hasbulla",
+      category: "Pizza",
+      descripcion: "En honor al 1",
+      precio: "$990",
+      imagen: "./assets/img/Photo Menu-5.png",
+    },
+    {
+      nombre: "Leo Messi",
+      category: "Pizza",
+      descripcion: "¡De pie señores!",
+      precio: "$350",
+      imagen: "./assets/img/Photo Menu-6.png",
+    },
+    {
+      nombre: "Nick Gi",
+      category: "Pizza",
+      descripcion: "La que desaparece",
+      precio: "$Gratis",
+      imagen: "./assets/img/Photo Menu-7.png",
+    },
+    {
+      nombre: "Bennazianna",
+      category: "Pizza",
+      descripcion: "La más completa",
+      precio: "$3650",
+      imagen: "./assets/img/pizza_bennaziana.jpg",
+    },
+    {
+      nombre: "Tronco-Pizza",
+      category: "Pizza",
+      descripcion: "Para todo el día",
+      precio: "$870",
+      imagen: "./assets/img/pizza_tronco.jpg",
+    },
+    {
+      nombre: "Clásica",
+      category: "Hamburguesa",
+      descripcion: "La que nunca defrauda",
+      precio: "$450",
+      imagen: "./assets/img/hambur_clasica.jpg",
+    },
+    {
+      nombre: "Shack burger de queso",
+      category: "Hamburguesa",
+      descripcion: "Puro queso",
+      precio: "$550",
+      imagen: "./assets/img/hambur_queso.jpg",
+    },
+    {
+      nombre: "Ranchera",
+      category: "Hamburguesa",
+      descripcion: "Tapa arterias",
+      precio: "$600",
+      imagen: "./assets/img/hambur_ranchera.jpg",
+    },
+    {
+      nombre: "Vegetariana",
+      category: "Hamburguesa",
+      descripcion: "La vaca te agradece",
+      precio: "$400",
+      imagen: "./assets/img/hambur_vegetariana.jpg",
+    },
+    {
+      nombre: "Papas Provenzal",
+      category: "Napapuki",
+      descripcion: "Van como piña",
+      precio: "$360",
+      imagen: "./assets/img/papas_provenzal.jpg",
+    },
+    {
+      nombre: "Papas Dulces",
+      category: "Napapuki",
+      descripcion: "Probá algo diferente",
+      precio: "$400",
+      imagen: "./assets/img/papas_dulce.jpg",
+    },
+    {
+      nombre: "Papas Curly",
+      category: "Napapuki",
+      descripcion: "Papa con rulo",
+      precio: "$350",
+      imagen: "./assets/img/papas_curly.jpg",
+    },
+    {
+      nombre: "Papas Poutine",
+      category: "Napapuki",
+      descripcion: "Con queso derretido y salsa grevy",
+      precio: "$460",
+      imagen: "./assets/img/papas_poutine.webp",
+    },
+    {
+      nombre: "Papas Picante",
+      category: "Napapuki",
+      descripcion: "Picoso",
+      precio: "$410",
+      imagen: "./assets/img/papas_picante.jpg",
+    },
+    {
+      nombre: "La MR. Pit (Individual)",
+      category: "Individual",
+      descripcion: "Sólo para expertos",
+      precio: "$44",
+      imagen: "./assets/img/Photo Menu.png",
+    },
+    {
+      nombre: "¡Q'Jamone! (Individual)",
+      category: "Individual",
+      descripcion: "c/jamón crudo",
+      precio: "$44",
+      imagen: "./assets/img/Photo Menu-1.png",
+    },
+    {
+      nombre: "La Charly García (Individual)",
+      category: "Individual",
+      descripcion: "¡BASTA!",
+      precio: "$48",
+      imagen: "./assets/img/Photo Menu-2.png",
+    },
+    {
+      nombre: "La Maradona (Individual)",
+      category: "Individual",
+      descripcion: "¡Eterna!",
+      precio: "$57",
+      imagen: "./assets/img/Photo Menu-3.png",
+    },
+    {
+      nombre: "Picantovich (Individual)",
+      category: "Individual",
+      descripcion: "Pica 2 veces",
+      precio: "$94",
+      imagen: "./assets/img/Photo Menu-4.png",
+    },
+    {
+      nombre: "La Hasbulla (Individual)",
+      category: "Individual",
+      descripcion: "En honor al 1",
+      precio: "$124",
+      imagen: "./assets/img/Photo Menu-5.png",
+    },
+    {
+      nombre: "Leo Messi (Individual)",
+      category: "Individual",
+      descripcion: "¡De pie señores!",
+      precio: "$2",
+      imagen: "./assets/img/Photo Menu-6.png",
+    },
+    {
+      nombre: "Nick Gi (Individual)",
+      category: "Individual",
+      descripcion: "La que desaparece",
+      precio: "$Gratis",
+      imagen: "./assets/img/Photo Menu-7.png",
+    },
+    {
+      nombre: "Bennazianna (Individual)",
+      category: "Individual",
+      descripcion: "La más completa",
+      precio: "$457",
+      imagen: "./assets/img/pizza_bennaziana.jpg",
+    },
+    {
+      nombre: "Tronco-Pizza (Individual)",
+      category: "Individual",
+      descripcion: "Para todo el día",
+      precio: "$109",
+      imagen: "./assets/img/pizza_tronco.jpg",
+    },
+    {
+      nombre: "Wrap Jamón y Queso",
+      category: "Wrap",
+      descripcion: "Soy el wrap 1",
+      precio: "$200",
+      imagen: "./assets/img/wrap_jamon.jpg",
+    },
+    {
+      nombre: "Wrap Pollo",
+      category: "Wrap",
+      descripcion: "Soy el wrap 2",
+      precio: "$250",
+      imagen: "./assets/img/wrap_pollo.jpg",
+    },
+    {
+      nombre: "Wrap Pulled Pork",
+      category: "Wrap",
+      descripcion: "Soy el wrap 3",
+      precio: "$350",
+      imagen: "./assets/img/wrap_pork.jpg",
+    },
+    {
+      nombre: "Tacos",
+      category: "Mexican",
+      descripcion: "Te sentirás en México D.C.",
+      precio: "$600",
+      imagen: "./assets/img/mexican_tacos.jpg",
+    },
+    {
+      nombre: "Enchiladas",
+      category: "Mexican",
+      descripcion: "Se te hace agua la boca",
+      precio: "$450",
+      imagen: "./assets/img/mexican_enchiladas.jpg",
+    },
+    {
+      nombre: "Pozole",
+      category: "Mexican",
+      descripcion: "Especial para los días fríos de invierno",
+      precio: "$350",
+      imagen: "./assets/img/mexican_pozole.jpg",
+    },
+    {
+      nombre: "Quesadilla",
+      category: "Mexican",
+      descripcion: "Uno de los mejores manjares que existen",
+      precio: "$250",
+      imagen: "./assets/img/mexican_quesadilla.jpg",
+    },
+    {
+      nombre: "Totopos",
+      category: "Mexican",
+      descripcion: "Para cualquier momento del día",
+      precio: "$300",
+      imagen: "./assets/img/mexican_totopos.jpg",
+    },
+    {
+      nombre: "Increible Hulk",
+      category: "Batiduki",
+      descripcion: "Puras cosas verdes",
+      precio: "$300",
+      imagen: "./assets/img/batido_hulk.jpg",
+    },
+    {
+      nombre: "Airon Man",
+      category: "Batiduki",
+      descripcion: "Todo el power",
+      precio: "$320",
+      imagen: "./assets/img/batido_iron.jpg",
+    },
+    {
+      nombre: "Homero",
+      category: "Batiduki",
+      descripcion: "Babosos!",
+      precio: "$310",
+      imagen: "./assets/img/batido_homero.jpg",
+    },
+  ]
+};
 
-const container = document.querySelector(".products-cards");
+localStorage.setItem("products", JSON.stringify(products.data)); // lo envío al local storage
+
+const $productCard = document.querySelector(".products-cards");
+const $productPopular = document.querySelector(".products-popular");
+const $buttons = document.querySelectorAll(".button-value");
 // al cargar la página, quiero que se renderice el HTML de todas las pizzas
 window.addEventListener("load", () => pintarHTML());
+
 function pintarHTML() {
-  container.innerHTML = pizzas
-    .map((pizza) => {
+  $productPopular.innerHTML = popular.data.map((pizza) => {
       return `
       <div class="product-card">
         <img src="${pizza.imagen}" alt="">
@@ -77,3 +374,105 @@ function pintarHTML() {
     })
     .join("");
 }
+
+/* Filtrado de productos */
+createCards();
+
+function createCards(){
+  for (let i of products.data) {
+    //Creamos la Card
+    let card = document.createElement("div");
+    //Card tienen categorias y están ocultas inicialmente
+    card.classList.add("card", i.category, "hide");
+    //image div
+    let imgContainer = document.createElement("div");
+    imgContainer.classList.add("image-container");
+    //img tag
+    let image = document.createElement("img");
+    image.setAttribute("src", i.imagen);
+    imgContainer.appendChild(image);
+    card.appendChild(imgContainer);
+    //container-info
+    let container = document.createElement("div");
+    container.classList.add("product-info");
+    //top
+    let top = document.createElement("div");
+    top.classList.add("product-top")
+    container.appendChild(top);
+    //name div
+    let nameContainer = document.createElement("div");
+    nameContainer.classList.add("name-container");
+    top.appendChild(nameContainer);
+    //nombre
+    let name = document.createElement("h2");
+    name.innerText = i.nombre;
+    nameContainer.appendChild(name);
+  
+    //mid
+    let mid = document.createElement("div");
+    mid.classList.add("product-mid")
+    container.appendChild(mid);
+    //Descripcion
+    let description = document.createElement("p");
+    description.innerText = i.descripcion;
+    mid.appendChild(description);
+  
+    //after
+    let after = document.createElement("div");
+    after.classList.add("product-after")
+    container.appendChild(after);
+    //Precio
+    let precio = document.createElement("h3");
+    precio.innerText = i.precio;
+    after.appendChild(precio);
+    //Boton agregar
+    let agregar = document.createElement("a");
+    agregar.innerText = "Agregar";
+    agregar.setAttribute("href", "#agregar");
+    after.appendChild(agregar);
+  
+    card.appendChild(container);
+    $productCard.appendChild(card);
+  }
+}
+
+$buttons.addEventListener("click", () =>{
+  alert("hola");
+})
+
+
+//Parametro pasado del boton (categoria)
+function filterProduct(value) {
+  $buttons.forEach((button) => {
+    //Comprobamos si el valor del parametro es igual a lo que tiene el boton en su innerText.
+    if (value.toUpperCase() == button.innerText.toUpperCase()) {
+      button.classList.add("active");
+    } else {
+      button.classList.remove("active");
+    }
+  });
+
+  //Seleccionamos todas las cards
+  let $elements = document.querySelectorAll(".card");
+  //loop a través de las cards
+  $elements.forEach((element) => {
+    //Para mostrar todos los productos
+    if (value == "all") {
+      element.classList.remove("hide");
+    } else {
+      //Comprobamos si el elemento contiene la categoría pasada por parametro
+      if (element.classList.contains(value)) {
+        //Mostramos los elementos que tengan esa categoría
+        element.classList.remove("hide");
+      } else {
+        //Escondemos los otros elementos
+        element.classList.add("hide");
+      }
+    }
+  });
+}
+
+//Mostrar los productos inicialmente
+window.onload = () => {
+  filterProduct("");
+};
